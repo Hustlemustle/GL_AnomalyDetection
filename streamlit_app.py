63287a4c0f8475e214246bcd2537792e5ea2e0d4
Sisheng <111482688+Hustlemustle@users.noreply.github.com>
2024-07-09 13:38:49 -0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,24 @@
+
+import pandas as pd
 import streamlit as st
 
+# Load the Excel file
+@st.cache_data
+def load_data(file_path):
+    return pd.read_excel(file_path, sheet_name='GL')
+
 def main():
-    st.title('Basic Streamlit App')
+    st.title('GL Data Analysis')
 
-    # Simple text input
-    account_key = st.number_input('Enter Account_key', min_value=1)
-    details = st.text_input('Enter Details')
+    uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
+    if uploaded_file is not None:
+        gl_data = load_data(uploaded_file)
 
-    st.write("Account Key:", account_key)
-    st.write("Details:", details)
+        account_key = st.number_input('Enter Account_key', min_value=1)
+        details = st.text_input('Enter Details')
+
+        st.write("Account Key:", account_key)
+        st.write("Details:", details)
 
 if __name__ == '__main__':
     main()
